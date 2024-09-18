877c305ffd8a8947dc5fc363245987ce6d20ae23
GabiDourado <gabi.dourados2xd@gmail.com>
2024-09-18 12:16:33 -0300

    início do perfil

--- src/Pages/Perfil.js
+++ src/Pages/Perfil.js
@@ -1,4 +1,4 @@
-import { View, Text, StyleSheet, Button, Image } from 'react-native'
+import { View, Text, StyleSheet, Button, Image, TouchableOpacity } from 'react-native'
 import React, { useContext } from 'react'
 import { AuthContext } from '../Context/AuthContext'
 
@@ -9,31 +9,69 @@ export default function Inserir() {
       <View style={css.caixa}>
         <Image
           style={css.tinyLogo}
-          source={require("../../assets/LogoAppAchôCerta.png")}
+          source={require("../../assets/FotosComuniQ/LogoComuniQ.jpeg")}
         />
       </View >
-      <View style={css.caixa2}>
-        <Text style={css.text}>Perfil</Text>
-        <Button onPress={() => setLogado(false)} title='Sair'></Button>
+      <View>
+        <Text>Foto</Text>
       </View>
+      <View style={css.campo1}>
+        <Text>Nome</Text>
+        <Text>Jered Cleiton</Text>
+      </View>
+      <View style={css.campo1}>
+        <Text>Sobrenome</Text>
+        <Text>Da Silva</Text>
+      </View>
+      <View style={css.campo1}>
+        <Text>Apelido</Text>
+        <Text>Jered</Text>
+      </View>
+      <View style={css.campo1}>
+        <Text>Email</Text>
+        <Text>Jered.Cleiton@email.com</Text>
+      </View>
+      <View style={css.campo1}>
+        <Text>Telefone</Text>
+        <Text>99 999999999</Text>
+      </View>
+      <View style={css.campo1}>
+        <Text>CPF</Text>
+        <Text>44444444444</Text>
+      </View>
+      <View style={css.campo1}>
+        <Text>CEP</Text>
+        <Text>172800-000</Text>
+      </View>
+      <View style={css.campo1}>
+        <Text>Cidade</Text>
+        <Text>Pederneiras</Text>
+      </View>
+      <View style={css.campo1}>
+        <Text>Bairro</Text>
+        <Text>Aquele lá</Text>
+      </View>
+      <TouchableOpacity>
+        <Text>Editar Perfil</Text>
+      </TouchableOpacity>
+      <TouchableOpacity>
+        <Text>Sair da Conta</Text>
+      </TouchableOpacity>
     </View>
   )
 }
 const css = StyleSheet.create({
   container: {
-    backgroundColor: "#E6DACA",
+    backgroundColor: "#ffff",
     flexGrow: 1,
     color: "white",
     // justifyContent: "center",
     alignItems: "center",
   },
-  text: {
-    color: "white"
-  },
   caixa: {
     height: 95,
     width: "100%",
-    backgroundColor: "#C7BB9D",
+    backgroundColor: "#20343F",
     display: "flex",
     alignItems: "center",
     justifyContent: "center"
@@ -43,4 +81,8 @@ const css = StyleSheet.create({
     width: "25%",
     marginTop: 15,
   },
+  campo1: {
+    borderColor: "#000",
+    borderBottomWidth: 1
+  }
 })
