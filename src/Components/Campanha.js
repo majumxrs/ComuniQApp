b3db4a14fc65c83f4607f6ad442f95ed9ff38744
majumxrs <mariaescola06@gmail.com>
2024-10-01 09:53:22 -0300

    so para pegar as circulações

--- src/Components/Campanha.js
+++ src/Components/Campanha.js
@@ -1,16 +1,56 @@
 import { View, Text, StyleSheet, FlatList, Button, TouchableOpacity, Image, TextInput, image, SafeAreaView, ScrollView, StatusBar } from 'react-native'
 import React, { useContext, useEffect, useState } from 'react'
 
-function Campanha() {
+export default function Campanha({ setCampanhas }) {
     return (
         <>
             <SafeAreaView style={css.container}>
                 <ScrollView>
-                    
+                    <View style={css.caixa}>
+                        <Image
+                            style={css.tinyLogo}
+                            source={require("../../assets/FotosComuniQ/LogoComuniQ.jpeg")}
+                        />
+                    </View >
+                    <View style={css.CaixaPaiTresBtn} >
+                        <View  style={css.Btn}>
+                            <TouchableOpacity style={css.btn} onPress={() => { setCampanhas(false) }}>
+                                <Text style={css.Texto}></Text>
+                            </TouchableOpacity>
+                        </View>
+                        <View>
+                            <TouchableOpacity style={css.btn} onPress={() => { setCampanhas(false) }}>
+                                <Text style={css.Texto}></Text>
+                            </TouchableOpacity>
+                        </View>
+                        <View>
+                            <TouchableOpacity style={css.btn} onPress={() => { setCampanhas(false) }}>
+                                <Text style={css.Texto}></Text>
+                            </TouchableOpacity>
+                        </View>
+                    </View>
                 </ScrollView>
             </SafeAreaView>
         </>
     )
 }
 
-export default Campanha
+const css = StyleSheet.create({
+    container: {
+        flexGrow: 1,
+        width: "100%",
+    },
+    caixa: {
+        height: 95,
+        width: "100%",
+        backgroundColor: "#20343F",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center"
+    },
+    tinyLogo: {
+        height: 60,
+        width: "25%",
+        marginTop: 15,
+    },
+})
