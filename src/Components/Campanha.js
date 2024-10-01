b99f93cbcce4e69d7a1890ea130d8d5c8c69bec9
majumxrs <mariaescola06@gmail.com>
2024-10-01 09:28:02 -0300

    ksksksksk

---
+++ src/Components/Campanha.js
@@ -0,0 +1,16 @@
+import { View, Text, StyleSheet, FlatList, Button, TouchableOpacity, Image, TextInput, image, SafeAreaView, ScrollView, StatusBar } from 'react-native'
+import React, { useContext, useEffect, useState } from 'react'
+
+function Campanha() {
+    return (
+        <>
+            <SafeAreaView style={css.container}>
+                <ScrollView>
+                    
+                </ScrollView>
+            </SafeAreaView>
+        </>
+    )
+}
+
+export default Campanha
