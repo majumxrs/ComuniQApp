3e6b25faa695734983cd6f7b8764ad60aa07dec9
majumxrs <mariaescola06@gmail.com>
2024-11-13 14:54:52 -0300

    tentando fazer funcionar

--- src/Context/AuthContext.js
+++ src/Context/AuthContext.js
@@ -4,7 +4,7 @@ export const AuthContext = createContext(0);
 
 function AuthProvider({ children }) {
     const [id, setId] = useState()
-    const [logado, setLogado] = useState(false);
+    const [logado, setLogado] = useState(true);
     const [error, setError] = useState(null);
     const [user, SetUser] = useState(false);
     const [menRecupSenha, setMenReupSenha] = useState(true);
